9fb5e60897b737f153bf74cdb8ae4765fb392c5c
Winrina-Kater1 <pptnammun4712@gmail.com>
2025-12-18 11:46:33 +0700

    Fix merge conflict in test.py
    Removed merge conflict markers from test.py.

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 print("Hello Word")
 print("Hello Word")
-<<<<<<< HEAD
-print("Hello Word")
-=======
 print("Hello Word")
 print("Hello Word")
->>>>>>> c458c98251c73d208acbb0fdd6ff0d44d56e07a7
+print("Hello Word")
